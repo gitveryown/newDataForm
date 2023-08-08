4c94381cc2f69d7b2b02788d232daa4b3b89466d
gitveryown <1williambwrgiht@gmail.com>
2023-08-08 16:26:10 -0400

    delete btn complete

--- src/userform/Form.jsx
+++ src/userform/Form.jsx
@@ -1,16 +1,22 @@
 'use client'
 import { useState } from "react";
+import PhoneInput from "react-phone-number-input/input";
 import './form.css'
 
 
+
+
 export default function Form() { 
+    let index = 0
 
     const {log} = console
+    index++
+    
 
 // useState, set html input value and updates value as well
     const [input, setInput] = useState({first:'', last:'',phone:'', email:''})
     const [dataForm, setDataForm] = useState([])
-    // const [readOnly, setReadOnly] = useState("")
+    const [editData, setEditData] = useState(false)
 
    
     // want to use map to make a new array during each submit to the table
@@ -21,23 +27,36 @@ export default function Form() {
     setInput((previnput)=> ({...previnput, [name]:value})) 
   }
   
+  
+
     const handleSubmit = (e) => {
         e.preventDefault()
         setDataForm([...dataForm, {input}])
-        // log(input,'data we entered' )
-        // log('Object data array',dataForm)
+        setInput({first:'', last:'', phone:'', email:''})
+        
     }
 
     const deleteRow = (index) => {
         const total = [...dataForm]
         total.splice(index, 1)
         setDataForm(total)
-        log(input, "this row wants to be deleted!")
+        log("I deleted index", index )
+        
     }
 
     const editRow = (index) => {
-        log(index, "this row wants an edit")
+       
+        const access = dataForm[index].input
+        //how to set access data back to the input fields
+        setInput(access)
+        setEditData(true)
+        /* When data is resubmitted, data should only reflect that index */
+        
+
+        log( "This row wants an edit:",index, access)
+      
     }
+    
     
   
     return (
@@ -45,15 +64,15 @@ export default function Form() {
             <form onSubmit={handleSubmit}>
                 <label htmlFor="names" className="title">Name:</label>
                 <label> First Name </label>
-                <input  name="first" type="text" id="name" value={input.first} onChange={handleOnChange} />
+                <input  name="first" type="text" id="name" required value={input.first} onChange={handleOnChange} />
                 <label> Last Name</label>
-                <input name="last" type="text" id="name" value={input.last} onChange={handleOnChange} />
+                <input name="last" type="text" id="name" required value={input.last} onChange={handleOnChange} />
 
                 <label className="title">Phone:</label>
-                <input name="phone" type="tel" value={input.phone} onChange={handleOnChange} placeholder="(123) 456-7890" maxLength={10} pattern="[0-9]*" />
+                <input name="phone" type="tel" value={input.phone} required onChange={handleOnChange} placeholder="(123) 456-7890" maxLength="10" pattern="[0-9]*" />
 
                 <label className="title">Email:</label>
-                <input name="email" type="email" placeholder="ex:name@example.com" value={input.email} onChange={handleOnChange} />
+                <input name="email" type="email" placeholder="ex:name@example.com" required value={input.email} onChange={handleOnChange} />
                 <button className="submit" type="submit" > submit </button>
                 
                 
@@ -75,7 +94,7 @@ export default function Form() {
                         <td>{data.input.last}</td>
                         <td>{data.input.phone}</td>
                         <td>{data.input.email}</td>
-                        <td><button onClick={editRow}>Edit</button> <button onClick={deleteRow}>Delete</button></td>
+                        <td><button onClick={()=>editRow(index)}>Edit</button> <button onClick={()=>deleteRow(index)}>Delete</button></td>
 
 
                     </tr>
