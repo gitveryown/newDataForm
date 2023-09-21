e3700ea90571e877d1c7a9ea4e2289b56e96ba99
gitveryown <1williambwrgiht@gmail.com>
2023-09-20 21:34:54 -0400

    added more styling and duplicate entry alert

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,13 +1,57 @@
-import styles from './page.module.css'
-import Form from '@/userform/Form'
-
-
+import './page.css'
+import Form from "@/userform/Form";
 
 export default function Home() {
-  return (
-    <>
-      <Form />
-    </>
+
+
+  // const inputs = [
+  //   {
+  //     id: 1,
+  //     name: "first",
+  //     type: "text",
+  //     placeholder: "First Name",
+  //     errorMessage:
+  //       "Name should be 3-16 characters and shouldn't include any special character!",
+  //     pattern: "^[A-Za-z0-9]{3,16}$",
+  //     required: true,
+  //   },
+  //   {
+  //     id: 2,
+  //     name: "email",
+  //     type: "email",
+  //     placeholder: "Email",
+  //     errorMessage: "It should be a valid email address!",
+  //     required: true,
+  //   },
+  //   {
+  //     id: 3,
+  //     name: "last",
+  //     type: "text",
+  //     require: true,
+  //     pattern: "^[A-Za-z0-9]{3,16}$",
+  //     errorMessage:
+  //       "Name should be 3-16 characters and shouldn't include any special character!",
+  //     placeholder: "Last Name",
+      
+  //   },
+  //   {
+  //     id: 4,
+  //     name: "phone",
+  //     type: "tel",
+  //     placeholder: "Enter Phone",
+  //     errorMessage:
+  //       "Enter a Valid Phone Number",
+  //     pattern: `[0-9]{10}`,
+  //     required: true,
+  //   },
     
-  )
+  // ];
+
+  return (
+    <div className="app">
+     
+      <Form />
+      
+    </div>
+  );
 }
--- src/userform/Form.jsx
+++ src/userform/Form.jsx
@@ -1,133 +1,170 @@
-'use client'
+"use client";
 import { useState } from "react";
-import './form.css'
+import "./form.css";
 
+export default function Form() {
+  const { log } = console;
 
+  const [input, setInput] = useState({
+    first: "",
+    last: "",
+    phone: "",
+    email: "",
+  });
+  const [dataForm, setDataForm] = useState([]);
+  //   const [inputError, setInputError] = useState(null);
 
+  const [editIndex, setEditIndex] = useState(-1);
 
-export default function Form() { 
-    let index = 0
+  const [duplicateError, setDuplicateError] = useState(false);
 
-    const {log} = console
-    index++
-    
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInput((previnput) => ({ ...previnput, [name]: value }));
+  };
 
-// useState, set html input value and updates value as well
-    const [input, setInput] = useState({first:'', last:'',phone:'', email:''})
-    const [dataForm, setDataForm] = useState([])
-   
-    const [editIndex, setEditIndex] = useState(-1)
-    
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const isDuplicateEmail = dataForm.some(
+      (contact) => contact.input.email === input.email
+    );
 
-
-
-   
-    // want to use arrray.map to make a NEW array during each submit to the table
-
-// function that handles updates on the state's object
-    const handleOnChange = (e) => {
-    const {name, value} = e.target
-    // e.target.name && e.target.value
-
-    setInput((previnput)=> ({...previnput, [name]:value})) 
-  }
-  
-  
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        if (editIndex !== -1){
-        // If editIndex is not -1, update existing entry
-        const updatedData = [... dataForm]
-        updatedData[editIndex] = { input }
-        setDataForm(updatedData)
-        setEditIndex(-1) // Reset editIndex
-        } else {
-        setDataForm( [...dataForm, { input }]) 
-        setInput ({ first: '', last: '', phone:'', email: '' })
-        }
+    if (isDuplicateEmail) {
+      alert("This email already exist, please edit or delete entry!");
+      setDuplicateError(true);
     }
-
-
-    const deleteRow = (index) => {
-
-        const row = [...dataForm]
-        row.splice(index, 1)/* only mutuating the NEW array from table 
+    if (editIndex !== -1) {
+      // If editIndex is not -1, update existing entry
+      const updatedData = [...dataForm];
+      updatedData[editIndex] = { input };
+      setDataForm(updatedData);
+      setEditIndex(-1); // Reset editIndex
+    } else {
+      setDataForm([...dataForm, { input }]);
+      setInput({ first: "", last: "", phone: "", email: "" });
+      setDuplicateError(false);
+    }
+  };
+  const deleteRow = (index) => {
+    const row = [...dataForm];
+    row.splice(index, 1); /* only mutuating the NEW array from table 
         not the Original from form */
-        
-        setDataForm(row)
-        log("I deleted index", index )
-        
-    }
 
-    const editRow = (index) => {
-       
-        const access = dataForm[index].input
-        
-        
-        
-        setInput(access) //how to set access data back to the input fields
+    setDataForm(row);
+    log("I deleted index", index);
+  };
 
-        setEditIndex(index) // set the index being edited
+  const editRow = (index) => {
+    const access = dataForm[index].input;
 
-        log( "This row wants an edit:",index, access, edit)
-      
-    }
-    
-    // option: if edit button is clicked return data back to index
-    // option: if editdata [index] isnt equal to dataform index return editdata back to index 
+    setInput(access); //how to set access data back to the input fields
 
-  
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="names" className="title">Name:</label>
-                <label> First Name </label>
-                <input  name="first" type="text" id="name" required value={input.first} onChange={handleOnChange} />
-                <label> Last Name</label>
-                <input name="last" type="text" id="name" required value={input.last} onChange={handleOnChange} />
+    setEditIndex(index); // set the index being edited
 
-                <label className="title">Phone:</label>
-                <input name="phone" type="tel" value={input.phone} required onChange={handleOnChange} placeholder="(123) 456-7890" maxLength="10" pattern="[0-9]*" />
+    log("This row wants an edit:", index, access);
+  };
 
-                <label className="title">Email:</label>
-                <input name="email" type="email" placeholder="ex:name@example.com" required value={input.email} onChange={handleOnChange} />
-                <button className="submit" type="submit" > submit </button>
-                
-                
-            </form>
+  // option: if edit button is clicked return data back to index
+  // option: if editdata [index] isnt equal to dataform index return editdata back to index
 
-            <table>
-            <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Phone Number</th>
-                <th>Email</th>
-                <th>Action</th>
-            </tr>
-            {
-               dataForm.map((data, index) => {
-                return(
-                    <tr key={index}>
-                        <td>{data.input.first}</td>
-                        <td>{data.input.last}</td>
-                        <td>{data.input.phone}</td>
-                        <td>{data.input.email}</td>
-                        <td><button onClick={()=>editRow(index)}>Edit</button> <button onClick={()=>deleteRow(index)}>Delete</button></td>
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        {/* <label htmlFor="names" className="title">
+          Name:
+        </label> */}
+        <input
+          label="Name"
+          name="first"
+          placeholder="First Name"
+          type="text"
+          title="Please enter a 3 character min"
+          required
+          pattern="^[A-Za-z0-9]{3,16}$"
+          value={input.first}
+          onChange={handleOnChange}
+        />
+        {/* <span>Minimum at least 3-16 characters</span> */}
 
+        <input
+          name="last"
+          placeholder="Last Name"
+          type="text"
+          pattern="^[A-Za-z0-9]{3,16}$"
+          required
+          title="Please enter a 3 characters minimum"
+          value={input.last}
+          onChange={handleOnChange}
+        />
 
-                    </tr>
-                )
-               }) 
-            }
-        </table>
-            
-       </>
-    )
-   
+        <label className="title">Phone:</label>
+        <input
+          name="phone"
+          type="tel"
+          value={input.phone}
+          required
+          title="Please enter a 10 digit minimum"
+          errormessage=""
+          onChange={handleOnChange}
+          placeholder="(123) 456-7890"
+          maxLength="10"
+          pattern="[0-9]{10}"
+        />
+        {/* <span>Invalid Phone Number</span><br></br> */}
+        <label className="title">Email:</label>
+        <input
+          name="email"
+          type="email"
+          placeholder="ex:name@example.com"
+          required
+          value={input.email}
+          onChange={handleOnChange}
+        />
+
+        {duplicateError && (
+          <p style={{ color: "red" }}>
+            Duplicate email entry. Please use a different email address.
+          </p>
+        )}
+
+        <button className="submit" type="submit">
+          submit
+        </button>
+      </form>
+
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Phone Number</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {dataForm.map((data, index) => {
+            return (
+              <tr key={index}>
+                <td>{data.input.first}</td>
+                <td>{data.input.last}</td>
+                <td>{data.input.phone}</td>
+                <td>{data.input.email}</td>
+                <td>
+                  <div>
+                    <button onClick={() => editRow(index)}>Edit</button>{" "}
+                    <button onClick={() => deleteRow(index)}>Delete</button>
+                  </div>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
+  );
 }
-
-
 
 // need to create the submit logic on the form, focus on the handleSubmit function specific to react.
 // once we get the data into an array we add that data to the table
+// if email doesnt equal dataform email array push it to dataform
